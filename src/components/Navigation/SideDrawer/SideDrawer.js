108e0bbd332fc9e3c06cd2c4cc20d98348986b6b
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-02-17 00:29:08 -0500

    changed folder structure

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import classes from './SideDrawer.css'
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/aux';
+import Aux from '../../../hoc/Aux/aux';
 
 const sideDrawer = (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Modal.css';
-import Aux from '../../../hoc/aux';
+import Aux from '../../../hoc/Aux/aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component{
