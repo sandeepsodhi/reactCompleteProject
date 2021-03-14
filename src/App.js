40683bb37c20776f36c3048dab424959a9056df9
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-03-13 20:11:18 -0500

    adding order summary page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
 
@@ -20,6 +21,7 @@ class App extends Component {
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
           {/* {this.state.show ? <BurgerBuilder /> : null} */}
         </Layout>
       </div>
