a32bbc260bde5f00488987055d326dd01858d1a9
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-03-18 00:50:16 -0400

    navigating back and forth at checkout page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -10,8 +10,8 @@ const checkoutSummary = (props) => {
             <div style={{width:'100%', margin: 'auto'}}>
                 <Burger ingredients= {props.ingredients} />
             </div>
-            <Button btnType = "Danger" >Cancel</Button>
-            <Button btnType = "Success" >Success</Button>
+            <Button btnType = "Danger" clicked={props.checkoutCancelled}>Cancel</Button>
+            <Button btnType = "Success" clicked={props.checkoutContinued}>Success</Button>
         </div>
     );
 }
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,12 +11,23 @@ class Checkout extends Component {
             bacon: 1
         }
     }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render () {
         return(
-            <CheckoutSummary ingredients={this.state.ingredients} /> 
+            <CheckoutSummary 
+            ingredients={this.state.ingredients} 
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler} /> 
         );
     }
- 
 }
 
 export default Checkout;
