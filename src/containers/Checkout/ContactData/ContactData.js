d439334e24fd301f477ecc186878e5b3b5f10a38
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-04-11 23:37:34 -0400

    working with order action and reducer

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import  * as actions from '../../../store/actions/index';
 
 class ContactData extends Component{
     state = {
@@ -90,15 +92,13 @@ class ContactData extends Component{
                 validation : {}
             }
         },
-        formIsValid : false,
-        loading: false
+        formIsValid : false
     }
 
     orderHandler = (event) => {
         event.preventDefault();
-        this.setState({loading:true});
-        const formData = {};
 
+        const formData = {};
         for(let formElementIdentifier in this.state.orderForm){
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;        
         }
@@ -108,16 +108,10 @@ class ContactData extends Component{
             price: this.props.price,
             orderData: formData
         }
-        axios.post('/orders.json', order) 
-            .then(response => {
-                this.setState({loading:false});
-                this.props.history.push('/');
-                // console.log(response);
-            })
-            .catch(error => {
-                this.setState({loading:false});
-                // console.log(error);
-        });
+
+        this.props.onOrderBurger(order);
+
+
     }
     checkValidity(value, rules){
         let isValid = true;
@@ -188,7 +182,7 @@ class ContactData extends Component{
             ))}
             <Button btnType="Success" disabled={!this.state.formIsValid} >ORDER</Button>
         </form>);
-        if(this.state.loading){
+        if(this.props.loading){
             form = <Spinner />
         }
 
@@ -205,8 +199,15 @@ class ContactData extends Component{
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     }
-}
+};
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+    return {
+        onOrderBurger : (orderData) => dispatch(actions.purchaseBurger(orderData))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
---
+++ src/store/actions/order.js
@@ -0,0 +1,37 @@
+import * as actionsTypes from './actionsTypes';
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionsTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData
+    }
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionsTypes.PURCHASE_BURGER_FAIL,
+        error: error
+    }
+};
+
+export const purchaseBurgerStart = () => {
+    return {
+        type: actionsTypes.PURCHASE_BURGER_START
+    }
+}
+
+export const purchaseBurger = (orderData) => {
+    return dispatch => {
+        dispatch(purchaseBurgerStart());
+        axios.post('/orders.json', orderData) 
+            .then(response => {
+                console.log(response.data);
+                dispatchEvent(purchaseBurgerSuccess(response.data, orderData));
+            })
+            .catch(error => {
+                dispatchEvent(purchaseBurgerFail(error));
+        });
+    }
+};
