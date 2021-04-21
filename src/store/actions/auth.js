baa30b526d87c0c5f2076d6234734e7e238f9c3a
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-04-20 22:51:30 -0400

    adding a authentication token from backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionsTypes';
 
 export const authStart = () => {
@@ -21,8 +23,22 @@ export const authFail = (error) => {
 };
 
 export const auth = (email, password) => {
-    return dipatch => {
-        dipatch(authStart());
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvA9ugtjWZnVVcNiJFYiaQq4PmDiy1sNY', authData)
+        .then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data));
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch(authFail(err));
+        });
     };
 };
 
