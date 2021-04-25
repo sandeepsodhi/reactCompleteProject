72682149aeddae980fa810d7431e4f07ea80e5bf
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-04-25 12:34:55 -0400

    added logout button and forwarding unauthenticated users

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,7 +34,12 @@ class BurgerBuilder extends Component{
     }
 
     purchaseHandler = () => {
-        this.setState({ purchasing: true});
+        if(this.props.isAuthenticated){
+            this.setState({ purchasing: true});
+        }else{
+            this.props.history.push('/auth');
+        }
+
     }
 
     purchaseCancelledHandler = () => {
@@ -68,6 +73,7 @@ class BurgerBuilder extends Component{
                         price = {this.props.price}
                         purchaseable = {this.updatePurchaseState(this.props.ings)}
                         ordered = {this.purchaseHandler}
+                        isAuth = {this.props.isAuthenticated }
                     />
                 </Aux>
             );
@@ -93,7 +99,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     };
 }
 
