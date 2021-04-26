08ac5b0c80295189d6f9a0518b41d8ce63e0de2c
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-04-25 22:20:35 -0400

    storing token value in local storage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,6 +24,9 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return  {
         type: actionTypes.AUTH_LOGOUT
     };
@@ -44,6 +47,24 @@ export  const setAuthRedirectPath = (path) => {
     };
 };
 
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if(!token){
+            dispatch(logout());
+        }else{
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if(expirationDate > new Date()){
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()))/1000);
+            }else{
+                dispatch(logout())
+            }
+        }
+    };
+};
+
 export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
@@ -59,6 +80,10 @@ export const auth = (email, password, isSignUp) => {
         axios.post(url, authData)
         .then(response => {
             console.log(response);
+            localStorage.setItem('token', response.data.idToken);
+            const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+            localStorage.setItem('expirationDate',expirationDate);
+            localStorage.setItem('userId', response.data.localId);
             dispatch(authSuccess(response.data.idToken, response.data.localId));
             dispatch(checkAuthTimeout(response.data.expiresIn));
         })
