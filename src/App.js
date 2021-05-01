7cdda7a1e9974cd8606f5dd890a1f611db8bd2d2
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-05-01 13:39:22 -0400

    fixing the redirect to front page issue

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
           <Route path="/checkout" exact component={Checkout} />
           <Route path="/orders" component={Orders} />
           <Route path="/logout" component={Logout} />
+          <Route path="/auth" component={Auth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
